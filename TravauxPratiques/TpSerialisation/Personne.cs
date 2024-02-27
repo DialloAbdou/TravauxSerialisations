12442726d8d42b89302200af1194f03f826edcb5
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-02-27 11:34:18 +0100

    initialisation projet sur la serialisation avec XML

---
+++ TravauxPratiques/TpSerialisation/Personne.cs
@@ -0,0 +1,10 @@
+﻿namespace TpSerialisation;
+
+public class Personne
+{
+  public string Nom { get; set; } = string.Empty;
+  public string Prenome { get; set; } =string.Empty;
+  public DateTime DateNaissance { get; set; }
+  public int Taille { get; set; }
+
+}
---
+++ TravauxPratiques/TpSerialisation/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml;
+using TpSerialisation;
+
+Console.WriteLine("======La serialisation======");
+
+// utilisation de setting
+
+var settings = new XmlReaderSettings
+{
+    IgnoreWhitespace = true,
+    IgnoreComments = true
+};
+
+Personne personne = new Personne();
+var reader = XmlReader.Create(@"C:\FormationC#\fondamentoC#\Serialisations\TravauxPratiques\TpSerialisation\fichier.xml",settings);
+
+reader.MoveToContent();
+
+reader.ReadStartElement("Personne");
+    reader.ReadStartElement("Nom");
+    personne.Nom = reader.ReadContentAsString();
+    reader.ReadEndElement();
+
+    reader.ReadStartElement("Prenom");
+    personne.Prenome = reader.ReadContentAsString();
+    reader.ReadEndElement();
+
+    reader.ReadStartElement("DateDeNaissance");
+    personne.DateNaissance = reader.ReadContentAsDateTime();
+    reader.ReadEndElement();
+
+    reader.ReadStartElement("Taille");
+    personne.Taille = reader.ReadContentAsInt();
+    reader.ReadEndElement();
+reader.ReadEndElement();
+
+Console.WriteLine($" Nom : {personne.Nom}, Prenom : {personne.Prenome}, DateNaissance : {personne.DateNaissance}, Taille : {personne.Taille}");
+Console.ReadLine();
